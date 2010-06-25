aded3eeb145b46eb1a649924412a78f631467f9b
Chanwit Kaewkasi <chanwit@gmail.com>
2010-06-25 15:47:05 +0700

    changed version to 1.0-M5

--- ZkEeGrailsPlugin.groovy
+++ ZkEeGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class ZkEeGrailsPlugin {
     // the plugin version
-    def version = "1.0-M4"
+    def version = "1.0-M5"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.2 > *"
     // the other plugins this plugin depends on
