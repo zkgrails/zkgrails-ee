ac54b343616cd48d540ce70d6f1f94b26193c51a
Chanwit Kaewkasi <chanwit@gmail.com>
2011-02-02 23:02:59 +0700

    upgrade ZK to 5.0.6-FL-2011-02-01

--- ZkEeGrailsPlugin.groovy
+++ ZkEeGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class ZkEeGrailsPlugin {
     // the plugin version
-    def version = "1.0.1"
+    def version = "1.0.2-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
